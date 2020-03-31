297bcff67f03f04f35a0d161e576026da5515eba
bushiweixiao <814912171@qq.com>
2020-03-31 19:53:39 +0800

    mall project

--- mall-user-service/src/main/resources/mapper/UmsMemberReceiveAddressMapper.java
+++
@@ -1,6 +0,0 @@
-package com.lsz.mall.user.mapper;
-
-import com.lsz.mall.bean.UmsMemberReceiveAddress;
-
-public interface UmsMemberReceiveAddressMapper extends tk.mybatis.mapper.common.Mapper<UmsMemberReceiveAddress>{
-}
